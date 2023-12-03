9767885c2c87325c41b88f48c8d3508a666442a4
Rubacova Polina <rubacova.polina@yandex.ru>
2023-12-03 15:16:27 +0300

    первичная структура проекта

---
+++ app.js
@@ -0,0 +1,17 @@
+const { PORT = 3000 } = process.env;
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
+
+// подключаем мидлвары, роуты и всё остальное...
+
+app.listen(PORT);
