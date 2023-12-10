c46a04d84fa8d3242bb9855fd4d3a700ec3edf57
Rubacova Polina <rubacova.polina@yandex.ru>
2023-12-10 17:59:34 +0300

    добавлеены контрол и роуты для пользователя

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const { PORT = 3000 } = process.env;
 
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -12,6 +16,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
     useFindAndModify: false
 });
 
-// подключаем мидлвары, роуты и всё остальное...
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT);
